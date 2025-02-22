ccf855538dbe83654545fc3ecccecf668e42a40d
erasernoob <1490910338@qq.com>
2025-02-22 11:55:57 +0800

    update: update the style of the button

--- src/pages/components/DynamicForm.jsx
+++ src/pages/components/DynamicForm.jsx
@@ -8,27 +8,30 @@ const DynamicForm = ({ datas, handleSubmit, tabs }) => {
   const  TabPane  = Tabs.TabPane
   const [form] = Form.useForm();
   const FormItem = Form.Item
+  console.log('')
 
 
 
 
   return (
+    <div className="form-wrapper">
     <Form form={form} layout="horizontal">
       <Tabs type="card" tabPosition="left" className='custom-tabs' size="large">
         {
           datas.map((category, index) => {
             const [categoryKey, data] = Object.entries(category)[0]
             return (
-          <TabPane title={tabsName[index]} key={categoryKey}>
+          <TabPane title={tabsName[index]} key={categoryKey} className='custom-tabsPane'>
             {Object.entries(data).map(([key, field]) => (
+              field.name.length <= 1000 ? 
               <FormItem
                 key={key}
                 label={field.name}
                 name={key}
                 rules={[{ required: true, message: `${field.name} 不能为空` }]}
               >
-                <Input style={{}}/>
-              </FormItem>
+                <Input className='input-component'/>
+              </FormItem> : <></>
             ))}
           </TabPane>
           )
@@ -37,10 +40,15 @@ const DynamicForm = ({ datas, handleSubmit, tabs }) => {
       </Tabs>
       
       {/* 提交按钮 */}
-      <Button type="secondary" style={{width: '200px', margin: '0 auto'}} onClick={() => handleSubmit()} >计算</Button>
-    </Form>
+      <div className="button-wrapper">
+      <Button type="primary" className='button submit-button'  onClick={() => handleSubmit()} >计算</Button>
+      <Button type="primary" className='button reset-button'  onClick={() => handleReset()} >重置</Button>
+      </div>
+   </Form>
 
+    </div>
   );
+
 };
 
 export default DynamicForm;
