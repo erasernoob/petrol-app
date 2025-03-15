73321025d3677641980534939a9c3310492e00f2
erasernoob <1490910338@qq.com>
2025-03-15 17:38:41 +0800

    🔖 release: ready for 0.5.0 version

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,7 +8,7 @@ use std::path::PathBuf;
 #[cfg(windows)]
 use std::os::windows::process::CommandExt;
 
-use std::process::{Child, Command};
+use std::process::{Child, Command, Stdio};
 use tauri::path::BaseDirectory;
 use tauri::Manager;
 
@@ -35,6 +35,8 @@ fn start_backend(backend_path: &PathBuf) -> std::io::Result<Child> {
 #[cfg(windows)]
 fn start_backend(backend_path: &PathBuf) -> std::io::Result<Child> {
     Command::new(&backend_path)
+        .stdout(Stdio::piped()) // 关键：重定向 stdout
+        .stderr(Stdio::piped()) // 关键：重定向 stderr
         .creation_flags(0x08000000)
         .spawn()
 }
@@ -45,23 +47,24 @@ fn main() {
         .plugin(tauri_plugin_fs::init())
         .setup(|app| {
             // Determine the backend executable name based on the OS
-            // let file_name = format!("backend{}", env::consts::EXE_SUFFIX); // Automatically adds .exe on Windows
+            let file_name = format!("backend{}", env::consts::EXE_SUFFIX); // Automatically adds .exe on Windows
 
-            // let backend_name = PathBuf::from("bin").join(file_name);
-            // let backend_path = app
-            //     .path()
-            //     .resolve(&backend_name, BaseDirectory::Resource)
-            //     .expect("Failed to resolve backend executable path");
+            let backend_name = PathBuf::from("bin").join(file_name);
+            let backend_path = app
+                .path()
+                .resolve(&backend_name, BaseDirectory::Resource)
+                .expect("Failed to resolve backend executable path");
 
-            // println!("backend_path {}", backend_path.display());
+            println!("backend_path {}", backend_path.display());
 
-            // let backend_process = start_backend(&backend_path).expect("start backend process failed");
+            let backend_process = start_backend(&backend_path).expect("start backend process failed");
 
-            // // Spawn the backend process
-            // app.manage(BackendProcess(backend_process));
+            // Spawn the backend process
+            app.manage(BackendProcess(backend_process));
             // println!("Tauri application started, backend is running.");
             Ok(())
         })
+        .plugin(tauri_plugin_shell::init()) // 确保启用 shell 插件
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
